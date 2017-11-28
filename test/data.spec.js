11ccd7fad203de247a8bd03a5ba32983918c2fa2
mkg20001 <mkg20001@gmail.com>
2017-11-28 17:41:46 +0100

    test: Add more tests

--- test/data.spec.js
+++ test/data.spec.js
@@ -6,11 +6,36 @@ const util = require('./util')
 
 const Client = require('../src')
 
-it('can send pings', cb => {
+it('can send ping', cb => {
   let [client, server] = util.Duplex()
   /* server = */Client(server, {
     ping: (data, cb) => cb(null, {body: 'Pong!'})
   }, true)
   client = Client(client, {})
   client.request('ping', {}, cb)
+})
+
+it('can send pings', cb => {
+  let [client, server] = util.Duplex()
+  /* server = */Client(server, {
+    ping: (data, cb) => cb(null, {body: 'Pong!'})
+  }, true)
+  client = Client(client, {})
+  client.request('ping', {}, err => {
+    if (err) return cb(err)
+    client.request('ping', {}, cb)
+  })
+})
+
+it('can disconnect properly', cb => {
+  let [client, server] = util.Duplex()
+  server = Client(server, {
+    ping: (data, cb) => cb(null, {body: 'Pong!'})
+  }, true)
+  client = Client(client, {})
+  client.request('ping', {}, err => {
+    if (err) return cb(err)
+    client.disconnect()
+    server.once('end', () => cb())
+  })
 })
